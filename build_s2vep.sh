bb2e216ce116e7658d791a7f7d7ab463a73db78a
Cool Development <grapaul77@gmail.com>
2014-01-28 16:45:35 +0100

    version 1.5.7

--- build_s2vep.sh
+++ build_s2vep.sh
@@ -5,7 +5,8 @@
 #
 
 #KERNELVERSION
-export KERNELVER=1.5.6
+export KERNELVER=1.5.7
+export KBUILD_BUILD_VERSION=$KERNELVER+
 
 #TOOLCHAINDIRECTORY
 export CROSS_COMPILE=/home/cooldevelopment/android/cm10.2/prebuilts/gcc/linux-x86/arm/arm-linux-gnueabihf-4.8/bin/arm-linux-gnueabihf-
@@ -18,6 +19,9 @@ make -j8
 #MAKE FLASHABLE ZIP
 cp arch/arm/boot/zImage build_zip/kernel/zImage
 cp drivers/net/wireless/bcmdhd/dhd.ko build_zip/system/lib/modules/dhd.ko
+cp drivers/scsi/scsi_wait_scan.ko build_zip/system/lib/modules/scsi_wait_scan.ko
+cp drivers/char/broadcom/gist.ko build_zip/system/lib/modules/gist.ko
+cp drivers/char/broadcom/sigmorph.ko build_zip/system/lib/modules/sigmorph.ko
 cd build_zip
 rm custom_kernel-*.zip 
 zip -r custom_kernel-$KERNELVER.zip kernel system META-INF

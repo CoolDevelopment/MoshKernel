ce7ebd73d0075d484adeb931a0354fae96822523
Cool Development <grapaul77@gmail.com>
2014-02-15 12:58:36 +0100

    Add frandom support. Frandom needs to get built from source after running 'make mrproper'. Now the frandom sources are included and get built automatically. Thanks Ryuinferno

--- build_s2vep.sh
+++ build_s2vep.sh
@@ -15,12 +15,19 @@ make mrproper
 make custom_s2vep_defconfig
 make -j8
 
+#BUILD FRANDOM MODULE
+cd frandom-android
+make clean
+make
+cd ..
+
 #MAKE FLASHABLE ZIP
 cp arch/arm/boot/zImage build_zip/kernel/zImage
 cp drivers/net/wireless/bcmdhd/dhd.ko build_zip/system/lib/modules/dhd.ko
 cp drivers/scsi/scsi_wait_scan.ko build_zip/system/lib/modules/scsi_wait_scan.ko
 cp drivers/char/broadcom/gist.ko build_zip/system/lib/modules/gist.ko
 cp drivers/char/broadcom/sigmorph.ko build_zip/system/lib/modules/sigmorph.ko
+cp frandom-android/frandom.ko build_zip/system/lib/modules/frandom.ko
 cd build_zip
 rm MoshKernel-*.zip 
 zip -r MoshKernel-$KERNELVER.zip kernel system META-INF

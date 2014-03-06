e51cd18893f4f49514193c408d94c3c0e994238b
Cool Development <grapaul77@gmail.com>
2014-03-06 21:27:27 +0100

    reomve ramdisk for aosp-based roms

--- build_s2vep.sh
+++ build_s2vep.sh
@@ -5,8 +5,7 @@
 #
 
 #KERNELVERSION
-export KERNELVER=1.7.2
-export CONFIG_LOCALVERSION="$KERNELVER"
+export KERNELVER=1.8.5
 
 #TOOLCHAINDIRECTORY
 export CROSS_COMPILE=/home/cooldevelopment/android/cm11/prebuilts/gcc/linux-x86/arm/arm-cortex_a9-linux-gnueabihf/bin/arm-cortex_a9-linux-gnueabihf-
@@ -19,6 +18,11 @@ while [ $continue -eq "0" ]; do
 	read rom
 	if [ $rom -eq "0" ] || [ $rom -eq "1" ]; then 
 		continue=1
+		if [ $rom -eq "0" ]; then
+			romfolder="stock"
+		else
+			romfolder="aosp-based"
+		fi
 	else
 		echo "Wrong input!"
 		sleep 2
@@ -36,39 +40,37 @@ fi
 make -j8
 
 #MAKE FLASHABLE ZIP
-cp arch/arm/boot/zImage build_zip/zImage
-if [ ! -d build_zip/system/lib/modules ]; then
-	mkdir build_zip/system/lib/modules
+if [ $rom -eq "1" ]; then
+	cp arch/arm/boot/zImage build_zip/"$romfolder"/kernel/zImage
+else
+	cp arch/arm/boot/zImage build_zip/"$romfolder"/zImage
 fi
 
-cp drivers/net/wireless/bcmdhd/dhd.ko build_zip/system/lib/modules/dhd.ko
-cp drivers/scsi/scsi_wait_scan.ko build_zip/system/lib/modules/scsi_wait_scan.ko
-cp drivers/char/broadcom/gist.ko build_zip/system/lib/modules/gist.ko
-cp drivers/char/broadcom/sigmorph.ko build_zip/system/lib/modules/sigmorph.ko
+if [ ! -d build_zip/"$romfolder"/system/lib/modules ]; then
+	mkdir build_zip/"$romfolder"/system/lib/modules
+fi
+
+cp drivers/net/wireless/bcmdhd/dhd.ko build_zip/"$romfolder"/system/lib/modules/dhd.ko
+cp drivers/scsi/scsi_wait_scan.ko build_zip/"$romfolder"/system/lib/modules/scsi_wait_scan.ko
+cp drivers/char/broadcom/gist.ko build_zip/"$romfolder"/system/lib/modules/gist.ko
+cp drivers/char/broadcom/sigmorph.ko build_zip/"$romfolder"/system/lib/modules/sigmorph.ko
+cp drivers/char/frandom.ko build_zip/"$romfolder"/system/lib/modules/frandom.ko
 cd build_zip
 rm MoshKernel-*.zip
-find . -type f -name *~ -exec rm {} \;
+find . -type f \( -name *~ -name "boot.img" -name *.cpio.gz \) -exec rm {} \;
 
-if [ $rom -eq "1" ]; then
-	./tools/mkbootfs aosp-rd | gzip > ramdisk.cpio.gz
 
-else
-	./tools/mkbootfs stock-rd | gzip > ramdisk.cpio.gz
+cd "$romfolder"
+
+if [ $rom -eq "0" ]; then
+	./../tools/mkbootfs stock-rd | gzip > ramdisk.cpio.gz
+	./../tools/mkbootimg --kernel zImage --ramdisk ramdisk.cpio.gz --base 0xa2000000 --cmdline 'console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000' --pagesize 4096 -o boot.img
 fi
-
+	
 if [ $rom -eq "1" ]; then
-	./tools/mkbootimg --kernel zImage --ramdisk ramdisk.cpio.gz --base 0xa2000000 --cmdline 'console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000' --pagesize 4096 -o boot.img
+	zip -r ../MoshKernel-aosp-based-"$KERNELVER".zip kernel system META-INF
 else 
-	./tools/mkbootimg --kernel zImage --ramdisk ramdisk.cpio.gz --base 0xa2000000 --cmdline 'console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000' --pagesize 4096 -o boot.img
+	zip -r ../MoshKernel-stock-"$KERNELVER".zip boot.img system META-INF
 fi
 
-if [ $rom -eq "1" ]; then
-	zip -r MoshKernel-aosp-based-"$KERNELVER".zip boot.img system META-INF
-else 
-	zip -r MoshKernel-stock-"$KERNELVER".zip boot.img system META-INF
-fi
-
-rm zImage
-rm boot.img
-rm *.cpio.gz
-rm system/lib/modules/*.ko
+find . -type f \( -name *~ -name "boot.img" -name *.cpio.gz -name "zImage" \) -exec rm {} \;
